8c7a525277f33258b0338bd43381e670cebeb273
Başar Yüksel <basar@stadion.io>
2023-06-22 22:02:35 +0200

    Clean up debug statements

--- App.tsx
+++ App.tsx
@@ -50,10 +50,7 @@ export default function App() {
     if (clickCount >= CLICK_COUNT) {
       stopGame();
     }
-  }, [clickCount]);
-
-  console.log(elapsedTime);
-  
+  }, [clickCount]);  
 
   return (
     <>
